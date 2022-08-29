90012bb203ce23267afea24bf03d7ac74bd1e13a
CalvinTai0402 <calvintai0402@gmail.com>
2022-08-28 19:26:33 -0700

    removed getStatisProps

--- pages/universities.js
+++ pages/universities.js
@@ -1,26 +1,12 @@
 import React, { Fragment } from 'react';
 import UniversitiesGallery from '../components/Universities/UniversitiesGallery';
-// import Universities from '../components/Universities/Universities';
-import { getPublicUniversities } from '../network/lib/universities';
 
-export default function universities({ unis }) {
+export default function universities() {
   return (
     <Fragment>
-      {/* <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        <Universities unis={unis} />
-      </div> */}
       <div className="mx-auto max-w-screen-xl max-h-screen-xl">
         <UniversitiesGallery />
       </div>
     </Fragment>
   );
-}
-
-export async function getStaticProps() {
-  let res = await getPublicUniversities(500);
-  return {
-    props: {
-      unis: res.data,
-    },
-  };
 }
